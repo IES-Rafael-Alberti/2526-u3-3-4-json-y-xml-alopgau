f75d7d9a0b0402cee91509b791602e3c97a32d15
alopgau <alopezg@ieslabahia.es>
2025-11-28 20:49:57 +0100

    Documentación cambiada

--- src/main.py
+++ src/main.py
@@ -1,26 +1,38 @@
 import json
 import os
+def cargar_json():
+    """Carga el JSON"""
+    with open("datos_usuarios_orig.json", "r") as f:
+        archivo = json.load(f)
+        return archivo
 def mostrar_datos(archivo:dict) -> None:
     """Muestra los datos del json en consola
     Args:
         archivo (dict): JSON en forma de diccionario
 
     Returns:
-        None
+        False: Hay algún error
+        True: No hay errores
 
     """
     if len(archivo["usuarios"]) == 0:
         print("No hay usuarios listados")
-    print("---Contenido actual del JSON---")
-    for usuario in archivo["usuarios"]:
-        print(f"ID: {usuario.get("id")} Nombre: {usuario.get("nombre")} Edad: {usuario.get("edad")} ")
-    print("---Fin del contenido---")
-def inicializar_datos() -> bool | dict:
+        return False
+    else:
+        print("---Contenido actual del JSON---")
+        for usuario in archivo["usuarios"]:
+            print(f"ID: {usuario.get("id")} Nombre: {usuario.get("nombre")} Edad: {usuario.get("edad")} ")
+        print("---Fin del contenido---")
+        return True
+def inicializar_datos() -> bool:
     """Copia el contenido del archivo a un archivo datos_usuarios.json
+    Returns:
+        False: Hay algún error
+        True: No hay errores
     """
     try:
-        with open("datos_usuarios_orig.json", "r") as f:
-            archivo = json.load(f)
+        with open("datos_usuarios_orig.json", "r"):
+            pass
     except FileNotFoundError:
         print("No se ha encontrado el archivo JSON. Asegúrate de que esté en el directorio raíz")
         return False
@@ -29,7 +41,7 @@ def inicializar_datos() -> bool | dict:
         return False
     else:
         print(f"Datos inicializados desde 'datos_usuarios_orig.json' a 'datos_usuarios.json'.")
-        return archivo
+        return True
 def limpiar_consola():
         """ Limpia la consola"""
         if os.name == "nt":
@@ -61,7 +73,7 @@ def eliminar_usuario(archivo:dict) -> None:
             Returns:
                 None"""
     del [archivo["usuarios"][1]]
-print("Usuario con ID 2 eliminado")
+    print("Usuario con ID 2 eliminado")
 def actualizar_archivo(archivo:dict) -> None:
     """Crea un JSON nuevo con los datos actualizados
                Args:
@@ -73,21 +85,21 @@ def actualizar_archivo(archivo:dict) -> None:
     print("Operaciones completadas. Archivo actualizado.")
 def main():
     if inicializar_datos():
-        archivo = inicializar_datos()
-        mostrar_datos(archivo)
-        espera()
-        limpiar_consola()
-        actualizar_edad(archivo)
-        mostrar_datos(archivo)
-        espera()
-        limpiar_consola()
-        insertar_usuario(archivo)
-        mostrar_datos(archivo)
-        espera()
-        limpiar_consola()
-        eliminar_usuario(archivo)
-        mostrar_datos(archivo)
-        espera()
-        actualizar_archivo(archivo)
+        archivo = cargar_json()
+        if mostrar_datos(archivo):
+            espera()
+            limpiar_consola()
+            actualizar_edad(archivo)
+            mostrar_datos(archivo)
+            espera()
+            limpiar_consola()
+            insertar_usuario(archivo)
+            mostrar_datos(archivo)
+            espera()
+            limpiar_consola()
+            eliminar_usuario(archivo)
+            mostrar_datos(archivo)
+            espera()
+            actualizar_archivo(archivo)
 if __name__ == "__main__":
     main()
