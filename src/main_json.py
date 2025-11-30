f6d4e3abe3af1f3328220093f475df55d773074c
alopgau <alopezg@ieslabahia.es>
2025-11-30 12:01:59 +0100

    xml hecho

---
+++ src/main_json.py
@@ -0,0 +1,105 @@
+import json
+import os
+def cargar_json():
+    """Carga el JSON"""
+    with open("datos_usuarios_orig.json", "r") as f:
+        archivo = json.load(f)
+        return archivo
+def mostrar_datos(archivo:dict) -> None:
+    """Muestra los datos del json en consola
+    Args:
+        archivo (dict): JSON en forma de diccionario
+
+    Returns:
+        False: Hay algún error
+        True: No hay errores
+
+    """
+    if len(archivo["usuarios"]) == 0:
+        print("No hay usuarios listados")
+        return False
+    else:
+        print("---Contenido actual del JSON---")
+        for usuario in archivo["usuarios"]:
+            print(f"ID: {usuario.get("id")} Nombre: {usuario.get("nombre")} Edad: {usuario.get("edad")} ")
+        print("---Fin del contenido---")
+        return True
+def inicializar_datos() -> bool:
+    """Copia el contenido del archivo a un archivo datos_usuarios.json
+    Returns:
+        False: Hay algún error
+        True: No hay errores
+    """
+    try:
+        with open("datos_usuarios_orig.json", "r"):
+            pass
+    except FileNotFoundError:
+        print("No se ha encontrado el archivo JSON. Asegúrate de que esté en el directorio raíz")
+        return False
+    except json.JSONDecodeError:
+        print("Problemas al decodificar el JSON")
+        return False
+    else:
+        print(f"Datos inicializados desde 'datos_usuarios_orig.json' a 'datos_usuarios.json'.")
+        return True
+def limpiar_consola():
+        """ Limpia la consola"""
+        if os.name == "nt":
+            os.system("cls")
+        else:
+            os.system("clear")
+def espera():
+    return input("Presione Enter para continuar")
+def actualizar_edad(archivo:dict) -> None:
+    """Actualiza la edad de un usuario
+    Args:
+        archivo (dict): archivo entrante
+    Returns:
+        None"""
+    archivo["usuarios"][0]["edad"] = 26
+    print("Actualizada la edad del usuario con id 1")
+def insertar_usuario(archivo:dict) -> None:
+    """Inserta un usuario
+        Args:
+            archivo (dict): archivo entrante
+        Returns:
+            None"""
+    archivo["usuarios"].append(dict(id=3, nombre="Pedro", edad=40))
+    print("Usuario Pedro añadido con éxito")
+def eliminar_usuario(archivo:dict) -> None:
+    """Elimina un usuario
+            Args:
+                archivo (dict): archivo entrante
+            Returns:
+                None"""
+    del [archivo["usuarios"][1]]
+    print("Usuario con ID 2 eliminado")
+def actualizar_archivo(archivo:dict) -> None:
+    """Crea un JSON nuevo con los datos actualizados
+               Args:
+                   archivo (dict): archivo entrante
+               Returns:
+                   None"""
+    with open("datos_usuarios_procesados.json", "w") as f:
+            json.dump(archivo,f, indent=4 )
+    print("Operaciones completadas. Archivo actualizado.")
+def main():
+    if inicializar_datos():
+        archivo = cargar_json()
+        if mostrar_datos(archivo):
+            espera()
+            limpiar_consola()
+            actualizar_edad(archivo)
+            mostrar_datos(archivo)
+            espera()
+            limpiar_consola()
+            insertar_usuario(archivo)
+            mostrar_datos(archivo)
+            espera()
+            limpiar_consola()
+            eliminar_usuario(archivo)
+            mostrar_datos(archivo)
+            espera()
+            actualizar_archivo(archivo)
+if __name__ == "__main__":
+    main()
--- src/main_xml.py
+++ src/main_xml.py
@@ -1,14 +1,22 @@
 
 import xml.etree.ElementTree as ET
 from main_json import espera, limpiar_consola
-def cargar_xml(nombre_fichero:str) -> ET.ElementTree | bool:
+def cargar_xml(destino:str) -> ET.ElementTree:
     """Carga el XML origen y lo convierte a árbol
     Args:
         nombre_fichero (str): Nombre del archivo XML
     Returns:
         ET.ElementTree: Árbol del XML
     """
-    return ET.parse(nombre_fichero)
+    try:
+        origen = ET.parse("datos_usuarios_orig.xml")
+    except (FileNotFoundError,ET.ParseError):
+        crear_arbol("usuarios")
+    else:
+        origen.write(destino, encoding="utf-8", xml_declaration=True)
+        return ET.parse(destino)
+
+
 def mostrar_datos(archivo:ET.ElementTree) -> bool:
     """Muestra los datos del XML en consola
     Args:
@@ -23,39 +31,41 @@ def mostrar_datos(archivo:ET.ElementTree) -> bool:
     raiz = archivo.getroot()
     if len(raiz.findall("usuario")) == 0:
         print("No hay usuarios listados")
+        return False
     else:
         print("---Contenido actual del XML---")
         for usuario in raiz.findall("usuario"):
             print(f"ID: {usuario.find("id").text} Nombre: {usuario.find("nombre").text} Edad: {usuario.find("edad").text} ")
         print("---Fin del contenido---")
-def crear_arbol(raiz:str) -> None:
+        return True
+def crear_arbol(str_raiz:str) -> None:
         """ Genera un XML con dicha raiz
             Args:
-                raiz (str): Nodo raiz
+                str_raiz (str): Nodo raiz
             Returns:
                 None
             """
-        r = ET.Element(raiz)
-        return r
+        raiz = ET.Element(str_raiz)
+        arbol_nuevo = ET.ElementTree(raiz)
+        arbol_nuevo.write("datos_usuarios.xml", encoding="utf-8", xml_declaration=True)
 
-def inicializar_datos(nombre_fichero:str) -> bool:
+def inicializar_datos(origen:str) -> bool:
     """Comprueba si hay algún error en el archivo
     Returns:
         False: Hay algún error
         True: No hay errores
     """
     try:
-        with open(nombre_fichero, "r"):
-            pass
+        arbol = ET.parse(origen)
     except FileNotFoundError:
-        print(f" El archivo {nombre_fichero} no existe Asegurate que esté en el directorio raiz.")
+        print(f" El archivo {origen} no existe Asegurate que esté en el directorio raiz.")
         return False
 
     except ET.ParseError:
         print("El archivo XML tiene un formato incorrecto.")
         return False
     else:
-        print(f"Datos inicializados desde '{nombre_fichero}' a 'datos_usuarios.xml'.")
+        print(f"Datos inicializados desde '{origen}' a 'datos_usuarios.xml'.")
         return True
 def actualizar_edad(archivo:ET.ElementTree) -> None:
     """Actualiza la edad de un usuario
@@ -63,7 +73,7 @@ def actualizar_edad(archivo:ET.ElementTree) -> None:
         archivo (ET.ElementTree): archivo entrante
     Returns:
         None"""
-    archivo.find("usuario").find("edad").text = 31
+    archivo.find("usuario").find("edad").text = "31"
     print("Actualizada la edad del usuario con id 1")
 
 def insertar_usuario(archivo:ET.ElementTree) -> None:
@@ -91,32 +101,35 @@ def eliminar_usuario(archivo:ET.ElementTree) -> None:
             raiz.remove(usuario)
     print("Usuario con ID 2 eliminado")
 def actualizar_archivo(archivo:ET.ElementTree) -> None:
-    """Crea un XML nuevo con los datos actualizados
+    """Actualiza el XML
                Args:
                    archivo (ET.ElementTree): archivo entrante
                Returns:
                    None"""
+    archivo.write("datos_usuarios.xml", encoding="utf-8", xml_declaration=True)
+    print("Archivo actualizado correctamente")
 
 
 
 def main():
     if inicializar_datos("datos_usuarios_orig.xml"):
-        archivo = cargar_xml("datos_usuarios_orig.xml")
-        mostrar_datos(archivo)
-        espera()
-        limpiar_consola()
-        actualizar_edad(archivo)
-        mostrar_datos(archivo)
-        espera()
-        limpiar_consola()
-        insertar_usuario(archivo)
-        mostrar_datos(archivo)
-        espera()
-        limpiar_consola()
-        eliminar_usuario(archivo)
-        mostrar_datos(archivo)
-        espera()
-        actualizar_archivo(archivo)
+        archivo = cargar_xml("datos_usuarios.xml")
+        if mostrar_datos(archivo):
+            espera()
+            limpiar_consola()
+            actualizar_edad(archivo)
+            mostrar_datos(archivo)
+            espera()
+            limpiar_consola()
+            insertar_usuario(archivo)
+            mostrar_datos(archivo)
+            espera()
+            limpiar_consola()
+            eliminar_usuario(archivo)
+            mostrar_datos(archivo)
+            espera()
+            actualizar_archivo(archivo)
+
 
 
 if __name__ == "__main__":
